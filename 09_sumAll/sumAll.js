8fed643990f86dd0a3b14cfbf2f4bc41a5d19666
oslifa <224970287+oslifa@users.noreply.github.com>
2025-09-20 22:56:03 +0100

    Implement sumAll function using conditions to validate input and a for loop

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    if (!Number.isInteger(numA) || !Number.isInteger(numB)) {
+        return "ERROR";
+    } else if (numA < 0 || numB < 0) {
+        return "ERROR";
+    }
+    
+    let start = numA;
+    let end = numB;
 
+    if (numB < numA) {
+        start = numB;
+        end = numA;
+    }
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
