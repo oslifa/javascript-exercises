119b56a83093310b856765c25d04fbf32a3081ba
oslifa <224970287+oslifa@users.noreply.github.com>
2025-09-20 22:05:13 +0100

    Implement removeFromArray function using filter method

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numbers) {
+    return array.filter(element => numbers.includes(element) ? false : true);
 };
 
 // Do not edit below this line
